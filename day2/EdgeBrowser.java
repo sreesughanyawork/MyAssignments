0180c35471afd01dbc9a38560c274a6a1e884d85
sreesughanyawork <151893151+sreesughanyawork@users.noreply.github.com>
2023-11-24 08:04:20 +0530

    Add files via upload
    Day 2

---
+++ day2/EdgeBrowser.java
@@ -0,0 +1,15 @@
+package week1.day2;
+
+
+public class EdgeBrowser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Browser br=new Browser();
+		br.launchBrowser("Edge");
+		br.loadUrl();
+		
+	}
+
+}
